2bd0b5d26cee2ea874d78c270cd7b8a0b7270a75
dawood zia <dawood@gtechsources.com>
2024-11-21 17:02:54 +0500

    dawood

---
+++ conversation.js
@@ -0,0 +1,12 @@
+import { View, Text } from "react-native";
+import React from "react";
+
+const conversation = () => {
+  return (
+    <View>
+      <Text>conversation</Text>
+    </View>
+  );
+};
+
+export default conversation;
